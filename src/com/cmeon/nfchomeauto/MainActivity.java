e2c20a931b441364efff439f5cacdad515b92cd9
Simeon Mugisha <cmeo226@yahoo.com>
2014-07-05 02:49:22 +0800

    Added buttons for accessing the options

--- src/com/cmeon/nfchomeauto/MainActivity.java
+++ src/com/cmeon/nfchomeauto/MainActivity.java
@@ -3,6 +3,7 @@ package com.cmeon.nfchomeauto;
 import android.app.Activity;
 import android.os.Bundle;
 import android.app.ActionBar;
+import android.view.View;
 
 public class MainActivity extends Activity
 {
@@ -11,6 +12,16 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+	ActionBar actionBar = getActionBar();
+	actionBar.hide();
         setContentView(R.layout.main);
     }
+
+    public void openVideosActivity(View view) {}
+
+    public void openMusicActivity(View view) {}
+
+    public void openPhotosActivity(View view) {}
+
+    public void openLightsActivity(View view) {}
 }
